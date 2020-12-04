17c59ade32de44239501aec9ecb9325e1d930db7
reddalexx <reddalexx@gmail.com>
2020-12-04 13:26:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.6.2',
+    version='0.6.2.1',
 
     description='Extract datetime objects from strings (LexPredict fork)',
     long_description=long_description,
